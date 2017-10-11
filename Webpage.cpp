a068fffc247f6226a3cc018955d41bf23a6e5473
jge162 <31228460+jge162@users.noreply.github.com>
2017-10-11 15:52:36 -0700

    Update Webpage.cpp

--- Webpage.cpp
+++ Webpage.cpp
@@ -1,3 +1,11 @@
+//
+//  Webpage.cpp
+//  Project II - C++ -CPSC131
+//
+//  Created by Jeremy Escobar on 10/10/17.
+//  Copyright © 2017 Jeremy Escobar. All rights reserved.
+//
+
 #include "Webpage.h"
 
 Webpage::Webpage() {
@@ -5,13 +13,20 @@ Webpage::Webpage() {
 }
 
 Webpage::Webpage(const string& webpageURL, const time_t& timeVisited) {
+    
+    this->webpageURL = webpageURL;
+    this->timeVisited = timeVisited;
+    
+    
     // TO BE COMPLETED
 }
 
 string Webpage::getURL() {
+    return webpageURL;
     // TO BE COMPLETED
 }
 
 time_t Webpage::getTime() {
+    return timeVisited;
     // TO BE COMPLETED
 }
