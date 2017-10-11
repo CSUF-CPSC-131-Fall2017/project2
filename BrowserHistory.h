e5002b05fc32df85a4ae78272b554f2634ea1acd
jge162 <31228460+jge162@users.noreply.github.com>
2017-10-11 15:56:12 -0700

    Update BrowserHistory.h

--- BrowserHistory.h
+++ BrowserHistory.h
@@ -1,3 +1,11 @@
+//
+//  BrowserHistory.hpp
+//  Project II - C++ -CPSC131
+//
+//  Created by Jeremy Escobar on 10/10/17.
+//  Copyright © 2017 Jeremy Escobar. All rights reserved.
+//
+
 #pragma once
 
 #include <iostream>
@@ -8,25 +16,42 @@
 #include "Webpage.h"
 using namespace std;
 
+class StringNode {
+    
+private:
+    string element;
+    StringNode *next;
+    
+    friend class BrowserHistory;
+};
+
 class BrowserHistory {
 public:
     BrowserHistory();
     ~BrowserHistory();
-
+    
     void visitSite(Webpage newSite);
+    
     string back();
     string forward();
-
+    
     void readHistory(string fileName);
-
+    
+    bool empty() const;
+    
     string getURL();
     size_t getNavSize();
-
+    
     void printBackSites();
     void printForwardSites();
     void printFullHistory();
-
+    
 private:
+    int n;
+    string URL;
+    size_t NavSize;
+    StringNode *head;
+    
     // Add private member variables for your class along with any
     // other variables required to implement the public member functions
     // TO BE COMPLETED
