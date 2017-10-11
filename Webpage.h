a04f3016d1d033f34be521ad3f9f68ace036f12d
jge162 <31228460+jge162@users.noreply.github.com>
2017-10-11 15:52:06 -0700

    Update Webpage.h

--- Webpage.h
+++ Webpage.h
@@ -1,3 +1,11 @@
+//
+//  Webpage.hpp
+//  Project II - C++ -CPSC131
+//
+//  Created by Jeremy Escobar on 10/10/17.
+//  Copyright © 2017 Jeremy Escobar. All rights reserved.
+//
+
 #pragma once
 
 #include <string>
@@ -9,8 +17,11 @@ public:
     Webpage(const string& webpageURL, const time_t& timeVisited);
     string getURL();
     time_t getTime();
-
+    
 private:
+    
+    string webpageURL;
+    time_t timeVisited;
     // Add private member variables for your class along with any
     // other variables required to implement the public member functions
     // TO BE COMPLETED
